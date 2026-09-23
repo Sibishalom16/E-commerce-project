de8cdd10859f2086eac0dcc1ae013f2ebc7452d5
Sibi Shalom <shalomsibi16@gmail.com>
2026-09-23 12:23:22 +0530

    fix: configure production cors and server port

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,46 @@
-const app = require("./app");
-const connectDatabase = require("./db/database");
-// const cors = require('cors');
-// app.use(cors({ origin: 'http://localhost:5173' }));
-
-// Handling uncaught Exception when setting up backend server
-process.on("uncaughtException", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log("shutting down the server for handling uncaught exception");
-});
-
 // config
-if (process.env.DB_URL  !== "PRODUCTION") {
+if (process.env.DB_URL !== "PRODUCTION") {
   require("dotenv").config({
     path: "config/.env",
   });
 }
 
+const app = require("./app");
+const connectDatabase = require("./db/database");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://e-commerce-project-plum-nine.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+// Handling uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("shutting down the server for handling uncaught exception");
+});
+
 // connect db
 connectDatabase();
 
 // create server
-const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${process.env.PORT}`
-  );
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.error(`Unhandled Rejection: ${err.message}`);
+  console.log("Shutting down the server due to unhandled promise rejection.");
 
-  // unhandled promise rejection(explain error handling when setting up server as you code)
-  process.on("unhandledRejection", (err) => {
-    console.error(`Unhandled Rejection: ${err.message}`);
-    console.log("Shutting down the server due to unhandled promise rejection.");
-    
-    server.close(() => {
-      process.exit(1); // Exit with failure code
-    });
+  server.close(() => {
+    process.exit(1);
   });
+});
